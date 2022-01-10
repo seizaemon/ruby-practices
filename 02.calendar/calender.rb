dfcc64bf0534851ee3e9c3415bbc1fa0ed45acdf
seizaemon <oden@venus.dti.ne.jp>
2022-01-10 21:37:41 +0900

    カレンダー問題の初期回答

---
+++ 02.calendar/calender.rb
@@ -0,0 +1,87 @@
+require "date"
+require "optparse"
+
+class Calendar
+  attr_reader :this_year, :this_month
+
+  EXIT_FAILURE = 1
+  Today = Date.today
+
+  def initialize(year, month)
+    @this_year = year
+    @this_month = month
+    exit EXIT_FAILURE if not are_params_valid?
+  end
+
+  # カレンダー表示
+  def display
+    puts display_title
+    puts display_day_of_the_week
+    puts display_days
+  end
+
+  # エラーチェック
+  private def are_params_valid?()
+    valid = true
+    if not (1 <= @this_year.to_i && @this_year.to_i <= 9999)
+      puts "year \`#{@this_year}\` not in range 1..9999"
+      valid = false
+    end
+    if not (1 <= @this_month.to_i && @this_month.to_i <= 12)
+      puts "#{@this_month} is neither a month number (1..12)"
+      valid = false
+    end
+    valid
+  end
+
+  # カレンダー年月タイトル生成
+  private def display_title
+    format("     %2s月 %4s年    ", @this_month.to_s, @this_year.to_s)
+  end
+
+  # カレンダー曜日生成
+  private def display_day_of_the_week
+    ["日", "月", "火", "水", "木", "金", "土"].join(" ")
+  end
+
+  # カレンダー日付生成
+  private def display_days
+    first_day = Date.new(@this_year.to_i, @this_month.to_i, 1)
+    last_day = Date.new(@this_year.to_i, @this_month.to_i, -1)
+
+    weeks = Array.new
+    num_of_week = 0
+    days = Array.new(7, "")
+
+    first_day.upto(last_day).each do |date|
+      if date == Date.today
+        # 当日に行き当たったら表示色を反転する
+        days[date.wday] = "\e[7m#{date.day.to_s}\e[0m"
+      else
+        days[date.wday] = date.day.to_s
+      end
+
+      # 週の終端、月の終端に至ったら出力
+      if date.wday == 6 || date == last_day
+        weeks[num_of_week] = format("% 2s % 2s % 2s % 2s % 2s % 2s % 2s", *days)
+        if date != last_day
+          # 次週に向けた初期化
+          num_of_week += 1
+          days = Array.new(7, "")
+        end
+      end
+
+    end
+    weeks.join("\n")
+  end
+end
+
+opt = OptionParser.new
+Today = Date.today
+params = {:year => Today.year, :month => Today.month}
+opt.on("-m VAL", "月を指定します") {|v| params[:month] = v if v != nil}
+opt.on("-y VAL", "年を指定します") {|v| params[:year] = v if v != nil}
+opt.parse!(ARGV)
+
+calendar = Calendar.new(params[:year], params[:month])
+calendar.display
