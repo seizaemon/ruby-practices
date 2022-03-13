5187e007e911f0f532bb96d79e35ce176144d135
seizaemon <oden@venus.dti.ne.jp>
2022-03-13 13:07:53 +0900

    feat: -l オプションの実装

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -4,6 +4,12 @@
 require 'optparse'
 require 'io/console/size'
 require 'pathname'
+require 'etc'
+require 'debug'
+
+# TODO
+# - 拡張属性マーク(@, +)の表示
+# - リファクタリング
 
 OUTPUT_MAX_COLUMNS = 3
 
@@ -11,6 +17,33 @@ class String
   # 空白に換算したときの文字幅を計算する
   def space_size
     chars.map { |c| c.ascii_only? ? 1 : 2 }.sum
+  end
+
+  def type_conv
+    types = {
+      '1' => 'p', # FIFO
+      '2' => 'c', # Character Special
+      '4' => 'd', # Directory
+      '6' => 'b', # Block Special
+      '10' => '-', # Regular file
+      '12' => 'l', # Symbolic link
+      '14' => 's' # Socket
+    }
+    types[self]
+  end
+
+  def mode_conv
+    mode = {
+      '0' => '---',
+      '1' => '--x',
+      '2' => '-w-',
+      '3' => '-wx',
+      '4' => 'r--',
+      '5' => 'r-x',
+      '6' => 'rw-',
+      '7' => 'rwx'
+    }
+    mode[self]
   end
 end
 
@@ -25,15 +58,17 @@ end
 def main
   # 出力する内容のフィルタフラグ
   filters = []
+  long_format = false
 
   opt = OptionParser.new
   # オプション処理
   opt.on('-a') { filters << 'SHOW_DOTMATCH' }
   opt.on('-r') { filters << 'SORT_REVERSE' }
+  opt.on('-l') { long_format = true }
 
   argv = opt.parse(ARGV)
 
-  ls = Ls.new(filters)
+  ls = long_format ? LsLong.new(filters) : Ls.new(filters)
 
   argv = ['./'] if argv.count.zero?
   ls.entries = argv
@@ -50,17 +85,13 @@ class Ls
 
   def output
     exclude_entries_nonexistent
-    files = @entries.select { |entry| File.file?(entry) }
-    dirs = @entries.select { |entry| File.directory?(entry) }
+    files = @entries.select { |entry| File.file?(entry) || !%r{/$}.match(entry) }
+    dirs = @entries.select { |entry| File.directory?(entry) && %r{/$}.match(entry) }
 
     # 実際のlsではファイルの出力が優先
     output_files(files)
     puts "\n" unless files.size.zero? || dirs.size.zero?
-    if files.size.zero?
-      output_dirs(dirs, force_label: (dirs.size != 1))
-    else
-      output_dirs(dirs, force_label: true)
-    end
+    files.size.zero? ? output_dirs(dirs, force_label: (dirs.size != 1)) : output_dirs(dirs, force_label: true)
   end
 
   def exclude_entries_nonexistent
@@ -89,10 +120,10 @@ class Ls
 
       puts "\n" if index.positive?
       puts "#{dir}:" if force_label
+      puts "total #{File.stat.new(dir).blocks}" if @filters.include?('LONG_FORMAT')
       next if entries_in_dir.size.zero?
 
       formatted = create_formatted_list(format_entries(entries_in_dir.switch_sort(reverse: @filters.include?('SORT_REVERSE'))))
-
       output_common(formatted)
     end
   end
@@ -154,6 +185,81 @@ class Ls
       end
     end
     columns
+  end
+end
+
+class LsLong < Ls
+  private
+
+  def output_files(files)
+    return if files.size.zero?
+
+    formatted = format_entries(files.switch_sort(reverse: @filters.include?('SORT_REVERSE')))
+    formatted.each { |line| puts line }
+  end
+
+  def output_dirs(dirs, force_label: false)
+    force_label = true if dirs.size > 1
+
+    dirs.switch_sort(reverse: @filters.include?('SORT_REVERSE')).each_with_index do |dir, index|
+      entries_in_dir = Dir.glob('*', dir_filter_flags_sum, base: dir, sort: true)
+
+      puts "\n" if index.positive?
+      puts "#{dir}:" if force_label
+      # 各ディレクトリのブロック数出力
+      puts "total #{File::Stat.new(dir).blocks}"
+
+      next if entries_in_dir.size.zero?
+
+      formatted = format_entries(entries_in_dir.switch_sort(reverse: @filters.include?('SORT_REVERSE')), dir)
+      puts formatted.join("\n")
+    end
+  end
+
+  def format_entries(entries, base_dir = '')
+    max_size_digit = get_max_size_digit(entries.map { |entry| (Pathname.new(base_dir) + entry).to_s })
+    max_nlink_digit = get_max_nlink_digit(entries.map { |entry| (Pathname.new(base_dir) + entry).to_s })
+
+    entries.map do |entry|
+      stat = File.lstat((Pathname.new(base_dir) + entry).to_s)
+      mode_arr = conv_mode(stat.mode)
+      nlink = format("%#{max_nlink_digit}d", stat.nlink)
+      # キャラクタデバイスの場合はデバイスタイプを表示
+      file_size = /[bc]/.match?(mode_arr[0]) ? "0x#{stat.rdev.to_s(16)}" : format("%#{max_size_digit}d", stat.size)
+      owner_group = "#{Etc.getpwuid(stat.uid).name}  #{Etc.getgrgid(stat.gid).name}"
+      mtime = Time.at(stat.mtime).strftime('%_m %_d %H:%M').to_s
+      file_str = mode_arr[0] == 'l' ? "#{entry} -> #{File.readlink((Pathname.new(base_dir) + entry).to_s)}" : entry
+
+      # 出力フォーマット
+      "#{mode_arr.join('')}  #{nlink} #{owner_group}  #{file_size} #{mtime} #{file_str}"
+    end
+  end
+
+  def conv_mode(mode)
+    mode_str = []
+    _, type, special, owner, group, other = /(.{1,2})(.)(.)(.)(.)/.match(mode.to_s(8)).to_a
+    _, setuid, setgid, sticky = /(.)(.)(.)/.match(format('%03d', special.to_i.to_s(2))).to_a
+    mode_str << type.type_conv
+    mode_str << (setuid == '1' ? special_filter(owner.mode_conv, 's') : owner.mode_conv)
+    mode_str << (setgid == '1' ? special_filter(group.mode_conv, 's') : group.mode_conv)
+    mode_str << (sticky == '1' ? special_filter(other.mode_conv, 't') : other.mode_conv)
+    mode_str
+  end
+
+  def special_filter(mode_str, special_flag)
+    case mode_str.match(/.$/)[0]
+    when '-' then mode_str.sub(/.$/, special_flag.upcase)
+    when 'x' then mode_str.sub(/.$/, special_flag.downcase)
+    end
+  end
+
+  def get_max_size_digit(entries)
+    # ファイルサイズ表記の最大桁数を取得する
+    entries.map { |entry| File.lstat(entry).size.to_s.chars.size }.max
+  end
+
+  def get_max_nlink_digit(entries)
+    entries.map { |entry| File.lstat(entry).nlink.to_s.chars.size }.max
   end
 end
 
--- 05.ls/test/test_tools.rb
+++ 05.ls/test/test_tools.rb
@@ -15,41 +15,43 @@ class TestTools
     @ja_dir_basename = '日本語のディレクトリ'
   end
 
-  def create_tmp_files(num_of_files, sub_dir: nil, is_ja: false, is_hidden: false)
+  def create_tmp_files(num_of_files, sub_dir: nil, is_ja: false, is_hidden: false, perm: 0o644)
     raise ArgumentError, "テストディレクトリ外にファイルは作成できません: #{sub_dir}" unless include?(sub_dir) || sub_dir.nil?
     raise Errno::ENOENT, "テストディレクトリ内にディレクトリが存在しません: #{sub_dir}" unless sub_dir.nil? || File.directory?("#{@test_dir}/#{sub_dir}")
 
     prefix = is_hidden ? '.' : ''
     basename = is_ja ? @ja_file_basename : @ascii_file_basename
-    create_tmp_files_common(num_of_files, "#{prefix}#{basename}", sub_dir_name: sub_dir)
+    create_tmp_files_common(num_of_files, "#{prefix}#{basename}", sub_dir_name: sub_dir, perm:)
   end
 
-  def create_tmp_files_common(num_of_files, basename, sub_dir_name: nil)
+  def create_tmp_files_common(num_of_files, basename, perm: 0o644, sub_dir_name: nil)
     return unless num_of_files.positive?
 
+    File.umask(0o000)
     (1..num_of_files).to_a.map do |n|
       file_name = sub_dir_name.nil? ? "#{basename}#{n}" : "#{sub_dir_name}/#{basename}#{n}"
-      File.open("#{@test_dir}/#{file_name}", 'w+') {}
-      file_name
+      File.open("#{@test_dir}/#{file_name}", 'w', perm) {}
+      Time.now.strftime('%_m %d %H:%M').to_s
     end
   end
 
-  def create_tmp_dirs(num_of_dirs, sub_dir: nil, is_ja: false, is_hidden: false)
+  def create_tmp_dirs(num_of_dirs, sub_dir: nil, is_ja: false, is_hidden: false, perm: 0o777)
     raise ArgumentError, "テストディレクトリ外にファイルは作成できません: #{sub_dir}}" unless include?(sub_dir) || sub_dir.nil?
     raise Errno::ENOENT, "テストディレクトリ内にディレクトリが存在しません: #{sub_dir}" unless sub_dir.nil? || File.directory?("#{@test_dir}/#{sub_dir}")
 
     prefix = is_hidden ? '.' : ''
     basename = is_ja ? @ja_dir_basename : @ascii_dir_basename
-    create_tmp_dirs_common(num_of_dirs, "#{prefix}#{basename}", sub_dir_name: sub_dir)
+    create_tmp_dirs_common(num_of_dirs, "#{prefix}#{basename}", sub_dir_name: sub_dir, perm:)
   end
 
-  def create_tmp_dirs_common(num_of_dirs, basename, sub_dir_name: nil)
+  def create_tmp_dirs_common(num_of_dirs, basename, sub_dir_name: nil, perm: 0o777)
     return unless num_of_dirs.positive?
 
+    File.umask(0o000)
     (1..num_of_dirs).map do |n|
       dir_name = sub_dir_name.nil? ? "#{basename}#{n}" : "#{sub_dir_name}/#{basename}#{n}"
-      Dir.mkdir("#{@test_dir}/#{dir_name}")
-      dir_name
+      Dir.mkdir("#{@test_dir}/#{dir_name}", perm)
+      Time.now.strftime('%_m %d %H:%M').to_s
     end
   end
 
@@ -80,5 +82,41 @@ class TestTools
     out.string
   ensure
     $stdout = STDOUT
+  end
+end
+
+class TestToolsLongFormat < TestTools
+  def create_symlink(from, to)
+    File.symlink(from, to)
+    Time.now.strftime('%_m %d %H:%M').to_s
+  end
+
+  def make_fifo
+    File.umask(0o000)
+    File.mkfifo("#{@test_dir}/test_fifo")
+    Time.now.strftime('%_m %d %H:%M').to_s
+  end
+
+  def ref_blk_file
+    # ブロックデバイスの作成にはroot権限が必要なので、存在するブロックデバイスの名前を探して返す
+    "brw-r-----  1 root  operator  0x1000000  3  4 16:55 /dev/disk0\n"
+  end
+
+  def ref_char_file
+    # キャラクタデバイスの作成にはroot権限が必要なので、存在するキャラクタデバイスの名前を探して返す
+    # /dev/nullはキャラクタデバイス
+    "crw-rw-rw-  1 root  wheel  0x3000003  3  4 16:55 /dev/zero\n"
+  end
+
+  def ref_socket_file
+    "srw-rw-rw-  1 root  daemon  0  3  4 16:55 /var/run/mDNSResponder\n"
+  end
+
+  def create_tmp_file_with_ctime(num_of_files, sub_dir: nil, is_ja: false, is_hidden: false, perm: 0o644)
+    create_tmp_files(num_of_files, sub_dir:, is_ja:, is_hidden:, perm:)
+  end
+
+  def create_tmp_dir_with_ctime(num_of_dirs, sub_dir: nil, is_ja: false, is_hidden: false, perm: 0o777)
+    create_tmp_dirs(num_of_dirs, sub_dir:, is_ja:, is_hidden:, perm:)
   end
 end
