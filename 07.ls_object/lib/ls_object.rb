35c6136c6c018c15a5e09f121d226eb0a80c40e2
seizaemon <oden@venus.dti.ne.jp>
2024-05-21 23:45:10 +0900

    fix: 本体をクラス化

---
+++ 07.ls_object/lib/ls_object.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require_relative 'ls_file_stat'
+require_relative 'screen'
+
+class LsObject
+  def initialize(paths, options)
+    @paths = paths.empty? ? ['.'] : paths
+    @options = options
+  end
+
+  def run
+    @options[:header] = !(@paths.length == 1 || @paths.empty?)
+
+    @screen_src = { '' => [] }
+    create_sorted_path_names(@paths).each do |path_name|
+      stat = LsFileStat.new(path_name)
+      if stat.file?
+        @screen_src[''] << stat
+      else
+        @screen_src[path_name.to_s] = create_stats_in_directory(path_name)
+      end
+    end
+
+    Screen.new(@screen_src, @options).show
+  end
+
+  private
+
+  def create_stats_in_directory(base_path_name)
+    file_paths = Dir.glob('*', (@options[:all_visible] ? File::FNM_DOTMATCH : 0), base: base_path_name.to_s)
+    file_paths << '..' if @options[:all_visible]
+    create_sorted_path_names(file_paths, base_path_name.to_s).map do |path_name|
+      LsFileStat.new(path_name)
+    end
+  end
+
+  def create_sorted_path_names(file_paths, base_path = '.')
+    sorted_paths = @options[:reverse] ? file_paths.sort.reverse : file_paths.sort
+    sorted_paths.map { |path| Pathname.new(base_path).join(path) }
+  end
+end
--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -2,46 +2,15 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require_relative 'lib/screen'
-require_relative 'lib/ls_file_stat'
+require_relative 'lib/ls_object'
 
-def main
-  options = {}
+options = {}
 
-  opt = OptionParser.new
-  opt.on('-a') { options[:all_visible] = true }
-  opt.on('-r') { options[:reverse] = true }
-  opt.on('-l') { options[:long_format] = true }
+opt = OptionParser.new
+opt.on('-a') { options[:all_visible] = true }
+opt.on('-r') { options[:reverse] = true }
+opt.on('-l') { options[:long_format] = true }
 
-  paths = opt.parse(ARGV)
-  options[:header] = !(paths.length == 1 || paths.empty?)
-  paths << '.' if paths.empty?
+ls_object = LsObject.new(opt.parse(ARGV), options)
 
-  screen_src = { '' => [] }
-
-  sort_paths(paths, reverse: options[:reverse]).each do |path_name|
-    stat = LsFileStat.new(path_name)
-    if stat.file?
-      screen_src[''] << stat
-    else
-      screen_src[path_name.to_s] = create_stats_in_directory(path_name, options)
-    end
-  end
-
-  Screen.new(screen_src, options).show
-end
-
-def create_stats_in_directory(base_path_name, options)
-  paths = Dir.glob('*', (options[:all_visible] ? File::FNM_DOTMATCH : 0), base: base_path_name.to_s)
-  paths << '..' if options[:all_visible]
-  sort_paths(paths, base_path_name.to_s, reverse: options[:reverse]).map do |path_name|
-    LsFileStat.new(path_name)
-  end
-end
-
-def sort_paths(paths, base_path = '.', reverse: false)
-  sorted_paths = reverse ? paths.sort.reverse : paths.sort
-  sorted_paths.map { |path| Pathname.new(base_path).join(path) }
-end
-
-main
+ls_object.run
